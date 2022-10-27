fd037bfe116c1ca5a93fbbd9ddfd6b0808ae4c4b
NsonguruaJohnson <nsonguruajohnson@gmail.com>
2022-10-27 10:25:00 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 9000;
+
+app.get('/api/info', (req, res) => {
+    res.status(200).json({
+        slackUsername: 'Nsongurua',
+        backend: true,
+        age: 23,
+        bio: 'I am a backend developer enthusiastic about solving problems'
+      });
+});
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}`);
+});
+
