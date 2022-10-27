24112dac0254303ab38e4de258f0740a604904e6
NsonguruaJohnson <nsonguruajohnson@gmail.com>
2022-10-27 10:28:30 +0100

    Edited endpoint

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 9000;
 
-app.get('/api/info', (req, res) => {
+app.get('/', (req, res) => {
     res.status(200).json({
         slackUsername: 'Nsongurua',
         backend: true,
